e993695241c08a98e9c645c10b57116da15b7a0b
dybalszymon <szymondybal23@gmail.com>
2026-01-07 15:28:23 +0100

    DataFacade basics (_get)

--- F1_ANALYSIS/data/F1DataFacade.py
+++ F1_ANALYSIS/data/F1DataFacade.py
@@ -1,10 +1,26 @@
+import requests
+from urllib.parse import urljoin
 
 class F1DataFacade:
-    def __init__(self, data_source):
-        self.data_source = data_source
+    BASE_URL = "https://api.openf1.org/v1/"
+    def __init__(self):
+        self.session = requests.Session()
+
+    def _get(self, endpoint : str, params : dict = None   ):
+        url = urljoin(self.BASE_URL, endpoint);
+
+        try:
+            response = self.session.get(url, params=params, timeout=100)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error: blad polaczenia z API: {e}")
+            return[]
 
     def get_lap_times(self):
+
         return self.data_source.fetch_lap_times()
+
 
     def get_telemetry_data(self):
         return self.data_source.fetch_telemetry_data()
